41e5e17b3ba94dc7418d515c72b65f0f667d0fa8
bailiang <15691023652@163.com>
2020-09-30 08:57:11 +0800

    删除main.exe

--- 二叉树/二叉树的线索化/中序/main.cpp
+++ 二叉树/二叉树的线索化/中序/main.cpp
@@ -54,7 +54,82 @@ void CreateInThread(ThreadTree T)
         InThread(T);
         if (pre->rchild == NULL)
         {
-            pre->rtag = 1;//处理遍历的最后一个结点
+            pre->rtag = 1; //处理遍历的最后一个结点
         }
     }
+}
+
+/**
+ * @description:  找到以p为根的子树中，第一个被中序遍历的结点
+ * @param {type} 
+ * @return {type} 
+ */
+ThreadNode *FirstNode(ThreadNode *p)
+{
+    //循环找到最左下结点(不一定是叶结点)
+    while (p->ltag == 0)
+        p = p->lchild;
+    return p;
+}
+
+/**
+ * @description:  在中序线索二叉树中找到结点p的后继结点
+ * @param {type} 
+ * @return {type} 
+ */
+ThreadNode *NextNode(ThreadNode *p)
+{
+    //右子树中最左下结点
+    if (p->rtag == 0)
+        return FirstNode(p->rchild);
+    else
+        return p->rchild; //rtag==1直接返回后继线索
+}
+
+/**
+ * @description:  对中序线索二叉树进行中序遍历(利用线索实现的非递归算法)
+ * @param {type}   
+ * @return {type} 
+ */
+void InOrder(ThreadNode *T) //空间复杂度O(1)
+{
+    for (ThreadNode *p = FirstNode(T); p != NULL; p = NextNode(p))
+        visit(p);
+}
+
+/**
+ * @description:  找到以p为根的子树中，最后一个被中序遍历的结点
+ * @param {type} 
+ * @return {type} 
+ */
+ThreadNode *LastNode(ThreadNode *p)
+{
+    //循环找到最右下结点(不一定是叶结点)
+    while (p->rtag == 0)
+        p = p->rchild;
+    return p;
+}
+
+/**
+ * @description: 在中序线索二叉树中找到结点p的前驱结点
+ * @param {type} 
+ * @return {type} 
+ */
+ThreadNode *PreNode(ThreadNode *p)
+{
+    if (p->ltag == 0)
+        return LastNode(p->lchild);
+    else
+        return p->lchild;
+}
+
+/**
+ * @description: 对中序线索二叉树进行逆向中序遍历
+ * @param {type} 
+ * @return {type} 
+ */
+void RevInOrder(ThreadNode *T)
+{
+    for (ThreadNode *p = LastNode(T); p != NULL; p = PreNode(p))
+        visit(p);
 }
